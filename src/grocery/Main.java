15eeea99eb08ff8d579fcf3a24a6b2b3e3471355
Gene Koval <kovale8@students.rowan.edu>
2019-01-30 00:05:04 -0500

    Added item purchase probability

--- src/grocery/Main.java
+++ src/grocery/Main.java
@@ -13,34 +13,103 @@ public class Main {
     private static final int WEEKEND_INCREASE = 50;
     private static final int DAYS_TO_RUN = 1;
 
+    // Item probabilities
+    // Milk
+    private static final int MILK = 70;
+    private static final int CEREAL_WITH_MILK = 50;
+    private static final int CEREAL_NO_MILK = 5;
+    // Baby food
+    private static final int BABY_FOOD = 20;
+    private static final int DIAPERS_WITH_BABY_FOOD = 80;
+    private static final int DIAPERS_NO_BABY_FOOD = 1;
+    // Bread
+    private static final int BREAD = 50;
+    // Peanut butter
+    private static final int PEANUT_BUTTER = 10;
+    private static final int JAM_WITH_PEANUT_BUTTER = 90;
+    private static final int JAM_NO_PEANUT_BUTTER = 5;
+
+    private static final Inventory inventory = new Inventory(PRICE_MULTIPLIER);
+    private static final Date date = new Date(START_DATE);
+    private static final Records records = new Records();
+
+    private static int customerCount;
+    private static int itemCount;
+
     public static void main(final String... args) {
-        final Inventory inventory = new Inventory(PRICE_MULTIPLIER);
-        final Date date = new Date(START_DATE);
-        final Records records = new Records();
-
         for (int day = 1; day <= DAYS_TO_RUN; day++, date.nextDay()) {
-            int customerCount = randRange(CUSTOMERS_LOW, CUSTOMERS_HIGH);
+            int customerTotal = randRange(CUSTOMERS_LOW, CUSTOMERS_HIGH);
             if (date.isWeekend())
-                customerCount += WEEKEND_INCREASE;
+                customerTotal += WEEKEND_INCREASE;
 
-            for (int customer = 1; customer <= customerCount; customer++) {
+            for (
+                    customerCount = 1;
+                    customerCount <= customerTotal;
+                    customerCount++) {
                 final int itemTotal = randRange(1, MAX_ITEMS);
+                itemCount = 0;
 
-                for (int itemCount = 0; itemCount <= itemTotal; itemCount++) {
-                    Product item = inventory.getItem();
+                if (chance(MILK)) {
+                    buyItem("milk");
 
-                    records.writeRecord(
-                        date.toString(),
-                        Integer.toString(customer),
-                        item.getSKU(),
-                        item.getPrice().toString()
-                    );
+                    if (chance(CEREAL_WITH_MILK))
+                        buyItem("cereal");
                 }
+                else if (chance(CEREAL_NO_MILK))
+                    buyItem("cereal");
+
+                if (chance(BABY_FOOD)) {
+                    buyItem("baby food");
+
+                    if (chance(DIAPERS_WITH_BABY_FOOD))
+                        buyItem("diapers");
+                }
+                else if (chance(DIAPERS_NO_BABY_FOOD))
+                    buyItem("diapers");
+
+                if (chance(BREAD))
+                    buyItem("bread");
+
+                if (chance(PEANUT_BUTTER)) {
+                    buyItem("peanut butter");
+
+                    if (chance(JAM_WITH_PEANUT_BUTTER))
+                        buyItem("jelly/jam");
+                }
+                else if (chance(JAM_NO_PEANUT_BUTTER))
+                    buyItem("jelly/jam");
+
+                // Buy random products to fill item total.
+                while (itemCount <= itemTotal)
+                    buyItem();
             }
         }
     }
 
+    private static void buyItem() {
+        recordSale(inventory.getItem());
+    }
+
+    private static void buyItem(final String type) {
+        recordSale(inventory.getItem(type));
+    }
+
+    private static boolean chance(final int probability) {
+        return randRange(1, 100) <= probability;
+    }
+
     private static int randRange(final int low, final int high) {
         return ThreadLocalRandom.current().nextInt(low, high + 1);
+    }
+
+    private static void recordSale(final Product product) {
+        itemCount++;
+
+        records.writeRecord(
+            date.toString(),
+            Integer.toString(customerCount),
+            product.getSKU(),
+            product.getPrice().toString()
+        );
     }
 }
